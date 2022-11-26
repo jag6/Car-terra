5212acfba4e04f6a4a8367fd126225328b848760
Matt <matt@randomforestweb.com>
2022-11-27 06:33:20 +0900

    mobile nav toggle

---
+++ static/js/nav-toggle.js
@@ -0,0 +1,25 @@
+const mobile_nav = document.getElementById('mobile-nav');
+const nav_overlay = document.getElementById('nav-overlay');
+const body = document.querySelector('body');
+
+const clickOff = () => {
+    mobile_nav.style.width = '0%';
+    nav_overlay.style.display = 'none';
+    body.style.overflow = 'auto';
+};
+
+document.getElementById('hamburger-icon').addEventListener('click', () => {
+    if(mobile_nav.style.width === '80%') {
+        clickOff();
+    }else {
+        mobile_nav.style.width = '80%';
+        nav_overlay.style.display = 'flex';
+        body.style.overflow = 'hidden';
+    }
+});
+
+nav_overlay.addEventListener('click', (e) => {
+    if(e.target == nav_overlay) {
+        clickOff();
+    }
+});
