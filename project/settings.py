a542f77be1b6d2574bc9498b888d4c4480767f27
Matt <matt@randomforestweb.com>
2022-11-27 23:38:37 +0900

    listings

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #post-installed
     'pages',
+    'listings',
+    'employees'
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
+    path('admin/', admin.site.urls)
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
